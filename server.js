6787ddcb566d16af9d76b4da28eb48e12a08ac1d
José Recinos <69140645+recinosjer@users.noreply.github.com>
2024-10-24 13:45:15 +0000

    generar random y id

--- server.js
+++ server.js
@@ -11,12 +11,33 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public/index.html'));
 });
 
+
+const  generarRandom = () => {
+  const num = 5;
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let result = "";
+    for (let i = 0; i < num; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+  return result;
+}
+
+
 io.on('connection', (socket) => {
   console.log('Nuevo cliente conectado');
+
+
   socket.on('chatmessage', (msg) => {
-    io.emit('chatmessage', msg);
+    id = socket.id
+    console.log (id)
+    io.emit('chatmessage', generarRandom(
+      
+    ) + " " + msg)
     console.log('message: ' + msg);
   });
+
   socket.on('disconnect', () => {
     console.log('Cliente desconectado');
   });
